334d18794087fcdcdd19dfd1b5db12f9ec4a48e3
lethienk69 <lethienk69@gmail.com>
2021-11-18 16:02:27 +0700

    add efconsole demo lan 1

---
+++ Thien/Efconsole/Efconsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace Efconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentRespository addStudent = new StudentRespository();
+            addStudent.AddStudent();
+        }
+    }
+}
---
+++ Thien/Efconsole/Efconsole/StudentRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Efconsole
+{
+    class StudentRespository
+    {
+        public void AddStudent()
+        {
+            Console.WriteLine("Hello World!");
+            var db = new StudentContext();
+            db.Add(new Student { StudentID = Guid.NewGuid().ToString(), StudentName = "Manh" });
+
+            db.SaveChanges();
+
+            var students = db.Students.ToList();
+
+            foreach (var s in students)
+            {
+                Console.WriteLine($"sid: {s.StudentID} , snam: {s.StudentName}");
+            }
+
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+    }
+}
