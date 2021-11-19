1ec028005c21d1995b5d2f570d93053c53ef6660
lethienk69 <lethienk69@gmail.com>
2021-11-19 17:21:25 +0700

    add CRUD lan 1

--- Thien/Efconsole/Efconsole/Program.cs
+++ Thien/Efconsole/Efconsole/Program.cs
@@ -7,8 +7,12 @@ namespace Efconsole
     {
         static void Main(string[] args)
         {
-            StudentRespository addStudent = new StudentRespository();
-            addStudent.AddStudent();
+            StudentRespository Studentress = new StudentRespository();
+            //Studentress.CreateStudent("nam2");
+             //Studentress.ReadStudent("020a7cbb-aedf-4eb4-b4f5-ce72a90e5ef0");
+            //Studentress.UpdateStudent("6612ee20-2eff-4b52-9535-d0c3c9cede8a", "thien2");
+           Studentress.DeleteStudent("8fe96a65-dd7d-405d-94e4-6ec0bee7b7b6");
+
         }
     }
 }
--- Thien/Efconsole/Efconsole/StudentRepository.cs
+++ Thien/Efconsole/Efconsole/StudentRepository.cs
@@ -7,23 +7,65 @@ namespace Efconsole
 {
     class StudentRespository
     {
-        public void AddStudent()
+        public void CreateStudent(string NameStudent)   //them hoc sinh theo ten
         {
-            Console.WriteLine("Hello World!");
             var db = new StudentContext();
-            db.Add(new Student { StudentID = Guid.NewGuid().ToString(), StudentName = "Manh" });
-
+            db.Add(new Student { StudentID = Guid.NewGuid().ToString(), StudentName = NameStudent });
             db.SaveChanges();
 
-            var students = db.Students.ToList();
+            /*  //in ra toàn bộ data
+            var students = db.Students.ToList();//db.Students.ToList() select data chuyển thành kiểu List
 
-            foreach (var s in students)
+            foreach (var s in students)                                            
             {
-                Console.WriteLine($"sid: {s.StudentID} , snam: {s.StudentName}");
+                Console.WriteLine($"sId: {s.StudentID} , sName: {s.StudentName}");   
             }
+            */
 
             Console.WriteLine("Done!");
+            Console.WriteLine("Press any key to continue");
             Console.ReadLine();
         }
+
+        public void ReadStudent(string id)  //lay hoc sinh theo id
+        {
+            var db = new StudentContext();
+            var students = db.Students.Where(i => i.StudentID == id);
+            foreach(var s in students)
+            {
+                Console.WriteLine("sId: " + s.StudentID +"  sName: " +s.StudentName);
+                // Console.WriteLine($"sId: {s.StudentID} , sName: {s.StudentName}");
+                
+            }
+            Console.WriteLine("Done!");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+        }
+        
+        public void UpdateStudent(string id, string name)   //cap nhat ten hoc sinh theo id
+        {
+            var db = new StudentContext();
+
+            var students= db.Students.Where(i => i.StudentID == id).FirstOrDefault();//????
+            students.StudentName = name;
+            db.SaveChanges();
+            Console.WriteLine("Done!");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+        }
+
+        public void DeleteStudent(string id)    //xoa hoc sinh theo id
+        {
+            var db = new StudentContext();
+            var students = db.Students.Where(i => i.StudentID == id).FirstOrDefault();
+            db.Students.Remove(students);
+            db.SaveChanges();
+            Console.WriteLine("Done!");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+        }
+
+        
+       
     }
 }
