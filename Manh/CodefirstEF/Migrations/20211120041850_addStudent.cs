8c1e6c9db7bc0951fff08b11adaad268608c8482
Manh Nguyen <themanh127@gmail.com>
2021-11-20 13:00:18 +0700

    them , sua , update student

---
+++ Manh/CodefirstEF/Migrations/20211120041850_addStudent.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.Data.EntityFrameworkCore.Metadata;
+
+namespace CodefirstEF.Migrations
+{
+    public partial class addStudent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Loai");
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    StudentName = table.Column<string>(nullable: true),
+                    Class = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.CreateTable(
+                name: "Loai",
+                columns: table => new
+                {
+                    MaLoai = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    MoTa = table.Column<string>(type: "text", nullable: true),
+                    TenLoai = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Loai", x => x.MaLoai);
+                });
+        }
+    }
+}
---
+++ Manh/CodefirstEF/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CodefirstEF.Models
+{   
+   public class Student
+    {  
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string Class { get; set; }
+
+    }
+}
