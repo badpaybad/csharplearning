d4aacad94c3dd98ba9519dc4a50e600f238b8830
Manh Nguyen <themanh127@gmail.com>
2021-11-29 14:20:24 +0700

    test mongoDb

---
+++ Manh/EntityFrameWorkJoin/MongoModels/MongoDBContext.cs
@@ -0,0 +1,17 @@
+﻿using RobotCloud.CoreMongoDb;
+using RobotCloud.CoreMongoDb.MNVN;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameWorkJoin.MongoModels
+{
+    public class MongoDBContext : BaseMongoUseObjectIdDbContext
+    {
+        public MongoDBContext() : base("118.70.117.208", "ManhTest", "sysadmin", "1234a@A") { }
+
+        public DbSetUseObjectId<StudentInformation> StudentInformation { get; set; }
+        public DbSetUseObjectId<ClassStudent> ClassStudent { get; set; }
+        public DbSetUseObjectId<Classes> Classes { get; set; }
+    }
+}
