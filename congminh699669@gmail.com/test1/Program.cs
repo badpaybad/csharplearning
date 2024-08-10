827c682e9d1c9ab2b8a52bd0a4261acbfdeffaa2
minhnc <minhnc@omt.com.vn>
2024-08-10 11:42:48 +0700

    minhnc thuc hanh 240810

---
+++ congminh699669@gmail.com/test1/Program.cs
@@ -0,0 +1,20 @@
+﻿int excercise1(int n) {
+    int sum = n *  (n+1) / 2;
+    return sum;
+}
+
+int excercise2(int n) {
+    int sum = n *  (n+1) * (2*n + 1) / 6;
+    return sum;
+}
+
+double excercise3(double n) {
+    double sum = 0;
+    for(double i = 1; i <= n; i++){
+        sum += 1/i;
+    }
+    return Math.Round(sum,2);
+}
+Console.WriteLine("Result exercise 1: " + excercise1(5));
+Console.WriteLine("Result exercise 2: " + excercise2(3));
+Console.WriteLine("Result exercise 3: " + excercise3(5));
