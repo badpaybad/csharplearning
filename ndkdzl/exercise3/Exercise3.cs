68c07d46e820cf641b293ebc9a7f5fbc4c731ecd
kiennd <kiennd@omt.vn>
2024-08-13 12:44:31 +0700

    exercise to 30

--- ndkdzl/exercise3/Exercise3.cs
+++ ndkdzl/exercise3/Exercise3.cs
@@ -1,4 +1,4 @@
-int exercise21(int n)
+﻿int exercise21(int n)
 {
     int total = 0;
     for (int i = 1; i <= n; i++)
@@ -50,15 +50,110 @@ int exercise24(int n)
     return count;
 }
 
-void exercise25(int n)
+//void exercise25(int n)
+//{
+//    Console.WriteLine("Cac so le thuoc uoc cua " + n + " la: ");
+//    for (int i = 1; i <= n; i++)
+//    {
+//        if (n % i == 0 && i % 2 != 0)
+//        {
+//            Console.WriteLine(i);
+//        }
+//    }
+//}
+
+int exercise25(int n){
+    int total = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0 && i % 2 == 0)
+        {
+            total += i;
+        }
+
+    }
+    return total;
+}
+
+int exercise26(int n)
 {
-    int count = 0;
+    int total = 1;
     for (int i = 1; i <= n; i++)
     {
         if (n % i == 0 && i % 2 != 0)
         {
-            Console.WriteLine("Cac so le thuoc uoc cua " + n + " la: " i);
+            total *= i;
         }
+
+    }
+    return total;
+}
+
+int exercise27(int n)
+{
+    int count = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0 && i % 2 == 0)
+        {
+            count++;
+        }
+
+    }
+    return count;
+}
+
+int exercise28(int n)
+{
+    int total = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            total += i;
+        }
+
+    }
+    return total;
+}
+
+int exercise29(int n)
+{
+    int max = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0 && i % 2 != 0)
+        {
+            max = i;
+        }
+
+    }
+    return max;
+}
+
+void exercise30(int n)
+{
+    // số hoàn thiện
+    // ví dụ: 6 , các ước số: 1 2 3 = 1 + 2 + 3 = 6 (ko tinh chinh no)
+    // ví dụ: 28 , các ước số: 1 2 4 7 14 = 1 + 2 + 4 + 7 + 14 = 28 (ko tinh chinh no)
+    int compare = 0;
+    for (int i = 1; i < n; i++)
+    {
+        
+        if (n % i == 0)
+        {
+            compare += i;
+        }
+
+    }
+    
+    if(compare == n)
+    {
+        Console.WriteLine(n + " la so hoan hao");
+    }
+    else
+    {
+        Console.WriteLine(n + " khong phai so hoan hao");
     }
 }
 
@@ -66,5 +161,10 @@ Console.WriteLine(exercise21(12));
 Console.WriteLine(exercise22(12));
 Console.WriteLine(exercise23(12));
 Console.WriteLine(exercise24(12));
-exercise25(12);
+Console.WriteLine(exercise25(12));
+Console.WriteLine(exercise26(5));
+Console.WriteLine(exercise27(12));
+Console.WriteLine(exercise28(12));
+Console.WriteLine(exercise29(100));
+exercise30(28);
 Console.ReadLine();
