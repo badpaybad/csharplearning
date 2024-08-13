1d3a65afa36753fd9ab430585ba91595b25b0f25
dungtm <dungtm@omt.vn>
2024-08-13 19:51:03 +0700

    exercise

---
+++ tranmanhdung1609@gmail.com/exercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            int Bai1(int n)
+            {
+                int sum = 0;
+                for (int i = 1; i <= n; i++)
+                {
+                    sum += i;
+                }
+                return sum;
+            }
+
+            double Bai2(int n)
+            {
+                int sum = 0;
+                for (int i = 1; i <= n; i++)
+                {
+                    sum += i * i;
+                }
+                return sum;
+            }
+
+            double Bai3(int n)
+            {
+                double sum = 0;
+                for (int i = 1; i <= n; i++)
+                {
+                    sum += 1 / i;
+
+                }
+            }
+        }
+    }
+}
+
