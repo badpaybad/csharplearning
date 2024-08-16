6ed510da394b09e47cffe88c11b91db4b987ffe4
dung <tranmanhdung1609@gmail.com>
2024-08-16 21:41:33 +0700

    exercise

--- tranmanhdung1609@gmail.com/exercise/Program.cs
+++ tranmanhdung1609@gmail.com/exercise/Program.cs
@@ -133,6 +133,285 @@ namespace Exercise
 
             return sum;
         }
+
+        static double Bai13(int n)
+        {
+            double sum = 0;
+            for(in  i = 1; int <= n; i++)
+            {
+                sum += Math.Pow(2, 2 * i);
+            }
+        }
+
+
+        static double Bai14(int n)
+        {
+            double sum = 0;
+            for (in  i = 0; int <= n; i++)
+            {
+                sum += Math.Pow(2, ((2 * i) + 1));
+            }
+        }
+
+
+        static double Bai15(int n)
+        {
+            double sum = 0
+            for (int i = 1; i <= n; i++)
+            {
+                double innerSum = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    innerSum += j;
+                }
+                sum += 1 / innerSum;
+            }
+
+            sum += 1;
+            return sum;
+        }
+        
+        static long Bai16(int n,int x)
+        {
+            double sum = x; // Khởi tạo sum với giá trị x (số hạng đầu tiên)
+
+            for (int i = 2; i <= n; i++)
+            {
+                double innerSum = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    innerSum += j;
+                }
+                sum += Math.Pow(x, i) / innerSum;
+            }
+            return sum;
+        }
+
+        static long GiaiThua(int n)
+        {
+            long result = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+
+        static double Bai17(int n, double x)
+        {
+            double sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                sum += Math.Pow(x, i) / GiaiThua(i);
+            }
+
+            return sum;
+        }
+
+
+        static double Bai19(int n, double x)
+        {
+            double sum = 1;
+
+            for (int i = 0; i <= n; i++)
+            {
+                sum += Math.Pow(x, (2* i)+1) / GiaiThua((2 * i) + 1);
+            }
+
+            return sum;
+        }
+
+        static double Bai18(int n, double x)
+        {
+            double sum = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                sum += Math.Pow(x, 2 * i) / GiaiThua(2 * i);
+            }
+
+            return sum;
+        }
+
+
+        static int[] Bai20(int n)
+        {
+            List<int> uocSoList = new List<int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    uocSoList.Add(i);
+                }
+            }
+
+            return uocSoList.ToArray();
+        }
+
+        static int Bai21(int n)
+        {
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+
+        static long Bai22(int n)
+        {
+            long product = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    product *= i;
+                }
+            }
+
+            return product;
+        }
+
+        static long Bai23(int n)
+        {
+            long product = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    product++;
+                }
+            }
+
+            return product;
+        }
+
+        static int[] bai24(int n)
+        {
+            List<int> array = new List<int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0 && i % 2 != 0)
+                {
+                    array.Add(i);
+                }
+            }
+
+            return array;
+        }
+
+        static double bai25(int n)
+        {
+            double sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0 && i % 2 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+
+        static double Bai26(int n)
+        {
+            double sum = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0 && i % 2 != 0)
+                {
+                    sum *= i;
+                }
+            }
+            return sum;
+        }
+
+        static double bai27(int n)
+        {
+            double soluong = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0 && i % 2 == 0)
+                {
+                    soluong++
+                }
+            }
+
+            return soluong;
+        }
+
+        static int Bai28(int n)
+        {
+            int sum = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+
+        static int bai29(int n)
+        {
+            int largestOdd = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0 && i % 2 != 0)
+                {
+                    if (i > largestOdd)
+                    {
+                        largestOdd = i;
+                    }
+                }
+            }
+
+            return largestOdd;
+        }
+
+        static string Bai30(int n)
+        {
+    
+            int sum = 0;
+
+            for (int i = 1; i <= n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            if (sum == n)
+            {
+                return n + " là số hoàn thiện";
+            }
+            else
+            {
+                return n + " là số không hoàn thiện";
+            }
+        }
+
     }
 }
 
