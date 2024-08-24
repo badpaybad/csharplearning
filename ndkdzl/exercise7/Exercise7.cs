19d16e208b92a792ca3faa7aeee0272a9725cb67
kiennd <kiennd@omt.vn>
2024-08-24 17:42:59 +0700

    exercise to 70

--- ndkdzl/exercise7/Exercise7.cs
+++ ndkdzl/exercise7/Exercise7.cs
@@ -82,8 +82,90 @@ void exercise65(int a, int b, int c) {
     }
 }
 
+void exercise66(int a, int b, int c)
+{
+    if (a == 0)
+    {
+        exercise64(b, c);
+    }
+    else
+    {
+        double delta = Math.Sqrt(Math.Pow(b, 2) - 4 * a * c);
+
+        if (delta > 0)
+        {
+            Console.WriteLine("Phuong trinh co 2 nghiem phan biet x1 = " + ((-b + delta) / (2*a*c)) + " x2 = " + ((-b - delta) / (2*a*c)));
+        }
+        else if (delta == 0)
+        {
+            Console.WriteLine("Phuong trinh co nghiem kep x1 = x2 = " + delta);
+        }
+        else
+        {
+            Console.WriteLine("Phuong trinh vo nghiem");
+        }
+    }
+}
+
+double exercise67(int x, int n){
+    double total = 0;
+    if (n < 1)
+    {
+        return n;
+    }
+    for (int i = 0; i <= n; i++) {
+        total += (Math.Pow(-1, i + 1) * Math.Pow(x, i));
+    }
+    return total / 1.0;
+}
+
+double exercise68(int x, int n)
+{
+    double total = 0;
+    if (n < 1)
+    {
+        return n;
+    }
+    for (int i = 0; i <= n; i++)
+    {
+        total += (Math.Pow(-1, i) * Math.Pow(x, 2*i));
+    }
+    return total / 1.0;
+}
+
+double exercise69(int x, int n)
+{
+    double total = 0;
+    if (n < 1)
+    {
+        return n;
+    }
+    for (int i = 0; i <= n; i++)
+    {
+        total += (Math.Pow(-1, i) * Math.Pow(x, (2*i)+1));
+    }
+    return total / 1.0;
+}
+
+double exercise70(int x, int n)
+{
+    double total = 0;
+    int seperate = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        seperate += i;
+        total += (Math.Pow(-1, i + 1) / seperate);
+    }
+
+    return total;
+}
 Console.WriteLine(exercise62(18,45));
 Console.WriteLine(exercise63(5,20));
 exercise64(5, 20);
 exercise65(3, 4, -7);
+exercise66(3, 4, -7);
+Console.WriteLine(exercise67(5, 3));
+Console.WriteLine(exercise68(5, 1));
+Console.WriteLine(exercise69(5, 1));
+Console.WriteLine(exercise70(5, 1));
 Console.ReadLine();
