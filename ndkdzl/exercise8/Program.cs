7a96ba6baca40a6fcbf0e18802eed9366a41e67c
KKoaLLa04 <ndkdzvl@gmail.com>
2024-08-26 00:18:53 +0700

    exericse to 80

--- ndkdzl/exercise8/Program.cs
+++ ndkdzl/exercise8/Program.cs
@@ -66,9 +66,87 @@ double exercise74(int x, int n)
     return total;
 }
 
+//exercise 75 (pending)
+//exercise 76 (pending)
+
+int exercise77(int n)
+{
+    int total = 0;
+    if(n > 0)
+    {
+        for (int i = 1; i <= n; ++i) {
+            total += i;
+        }
+
+        return total;
+    }
+
+    return 0;
+}
+
+int[] exercise78(int n)
+{
+    List<int> ucln = new List<int>();
+    if (n > 0)
+    {
+        for (int i = 1; i <= n; ++i)
+        {
+            if( n % i == 0)
+            {
+                ucln.Add(i);
+            }
+        }
+
+        return ucln.ToArray();
+    }
+
+    return new int[0];
+}
+
+void lietKeUocSo(int n)
+{
+    int[] uocSo = exercise78(n);
+
+    Console.WriteLine("Cac uoc so cua " + n + " La " + string.Join(", ", uocSo));
+}
+
+int exercise79(int n)
+{
+    int count = 0;
+    if (n > 0)
+    {
+        for (int i = 1; i <= n; ++i)
+        {
+            if (n % i == 0)
+            {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+
+double exercise80(int x, int n)
+{
+    double total = 0;
+    int divide= 0;
+    for(int i = 1; i <= n; i++)
+    {
+        divide += i;
+        total += Math.Pow(x, i) / divide;
+    }
+
+    return total;
+}
 
 Console.WriteLine(exercise71(5, 2));
 Console.WriteLine(exercise72(5, 2));
 Console.WriteLine(exercise73(4, 2));
 Console.WriteLine(exercise74(4, 1));
+Console.WriteLine(exercise77(4));
+//Console.WriteLine(exercise78(10));
+lietKeUocSo(10);
+Console.WriteLine(exercise79(10));
+Console.WriteLine(exercise80(2,3));
 Console.ReadLine();
