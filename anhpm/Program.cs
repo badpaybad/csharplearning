190d871eb6b8fdb84942dbb1b697feacd2f3e3af
phunganh23082 <phunganh23082k@gmail.com>
2024-08-27 00:28:14 +0700

    bai 1-10

---
+++ anhpm/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine($"bai 1 voi n = 1, ket qua: {SessionOne.bai1(1)}");
+Console.WriteLine($"bai 1 voi n = 0, ket qua: {SessionOne.bai1(0)}");
+Console.WriteLine($"bai 1 voi n = 9, ket qua: {SessionOne.bai1(9)}");
+Console.WriteLine();
+//
+Console.WriteLine($"bai 2 voi n = 1, ket qua: {SessionOne.bai2(1)}");
+Console.WriteLine($"bai 2 voi n = 0, ket qua: {SessionOne.bai2(0)}");
+Console.WriteLine($"bai 2 voi n = 9, ket qua: {SessionOne.bai2(9)}");
+Console.WriteLine();
+//
+Console.WriteLine($"bai 3 voi n = 1, ket qua: {SessionOne.bai3(1)}");
+Console.WriteLine($"bai 3 voi n = 0, ket qua: {SessionOne.bai3(0)}");
+Console.WriteLine($"bai 3 voi n = 9, ket qua: {SessionOne.bai3(9)}");
+Console.WriteLine();
+//
+Console.WriteLine($"bai 4 voi n = 1, ket qua: {SessionOne.bai4(1)}");
+Console.WriteLine($"bai 4 voi n = 0, ket qua: {SessionOne.bai4(0)}");
+Console.WriteLine($"bai 4 voi n = 9, ket qua: {SessionOne.bai4(9)}");
+Console.WriteLine();
+//
+Console.WriteLine($"bai 5 voi n = 1, ket qua: {SessionOne.bai5(1)}");
+Console.WriteLine($"bai 5 voi n = 0, ket qua: {SessionOne.bai5(0)}");
+Console.WriteLine($"bai 5 voi n = 9, ket qua: {SessionOne.bai5(9)}");
+Console.WriteLine();
+//
+Console.WriteLine($"bai 6 voi n = 1, ket qua: {SessionOne.bai6(1)}");
+Console.WriteLine($"bai 6 voi n = 0, ket qua: {SessionOne.bai6(0)}");
+Console.WriteLine($"bai 6 voi n = 9, ket qua: {SessionOne.bai6(9)}");
+Console.WriteLine();
+//
+Console.WriteLine($"bai 7 voi n = 1, ket qua: {SessionOne.bai7(1)}");
+Console.WriteLine($"bai 7 voi n = 0, ket qua: {SessionOne.bai7(0)}");
+Console.WriteLine($"bai 7 voi n = 9, ket qua: {SessionOne.bai7(9)}");
+Console.WriteLine();
+//
+Console.WriteLine($"bai 8 voi n = 1, ket qua: {SessionOne.bai8(1)}");
+Console.WriteLine($"bai 8 voi n = 0, ket qua: {SessionOne.bai8(0)}");
+Console.WriteLine($"bai 8 voi n = 9, ket qua: {SessionOne.bai8(9)}");
+Console.WriteLine();
+Console.ReadLine();
---
+++ anhpm/SessionOne.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+public class SessionOne
+{
+    public SessionOne()
+    {
+
+    }
+    public static int bai1(int n)
+    {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return n + bai1(n - 1);
+    }
+
+    public static int bai2(int n)
+    {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return (int)Math.Pow(n, 2) + bai2(n - 1);
+    }
+
+    public static float bai3(int n)
+    {
+        try
+        {
+            if (n == 0) throw new Exception("bai 3 value n=0 is invalid ");
+            if (n == 1) return 1;
+            return (float)(1f / n) + bai3(n - 1);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception caught: {ex.Message}");
+            return 0;
+        }
+    }
+
+    public static float bai4(int n)
+    {
+        try
+        {
+            if (n == 0) throw new Exception("bai 4 value n=0 is invalid ");
+            if (n == 1) return 1f / 2;
+            return (float)(1f / (2 * n)) + bai4(n - 1);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception caught: {ex.Message}");
+            return 0;
+        }
+    }
+
+    public static float bai5(int n)
+    {
+        try
+        {
+            if (n == 0) return 1;
+            return (float)(1f / (2 * n + 1)) + bai5(n - 1);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception caught: {ex.Message}");
+            return 0;
+        }
+    }
+
+    public static float bai6(int n)
+    {
+        try
+        {
+            if (n == 0) throw new Exception("bai 6 value 0 is invalid");
+            if (n == 1) return 1f / 2;
+            return (float)1f / (n * (n + 1)) + bai6(n - 1);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception caught: {ex.Message}");
+            return 0;
+        }
+    }
+
+    public static float bai7(int n)
+    {
+        try
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1f / 2;
+            return (float)1f * n / (n + 1) + bai7(n - 1);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception caught: {ex.Message}");
+            return 0;
+        }
+    }
+
+    public static float bai8(int n)
+    {
+        try
+        {
+            if (n == 0) return 1f / 2;
+            return (float)1f * (2 * n + 1) / (2 * n + 2) + bai8(n - 1);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception caught: {ex.Message}");
+            return 0;
+        }
+    }
+}
