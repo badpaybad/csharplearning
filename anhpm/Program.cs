600589a1c1f40c1528688a964c13457e377f63a4
phunganh23082 <phunganh23082k@gmail.com>
2024-08-27 01:08:37 +0700

    1-16

--- anhpm/Program.cs
+++ anhpm/Program.cs
@@ -39,4 +39,50 @@ Console.WriteLine($"bai 8 voi n = 1, ket qua: {SessionOne.bai8(1)}");
 Console.WriteLine($"bai 8 voi n = 0, ket qua: {SessionOne.bai8(0)}");
 Console.WriteLine($"bai 8 voi n = 9, ket qua: {SessionOne.bai8(9)}");
 Console.WriteLine();
+//
+Console.WriteLine($"bai 9 voi n = 1, ket qua: {SessionOne.bai9(1)}");
+Console.WriteLine($"bai 9 voi n = 0, ket qua: {SessionOne.bai9(0)}");
+Console.WriteLine($"bai 9 voi n = 9, ket qua: {SessionOne.bai9(9)}");
+Console.WriteLine();
+//
+Console.WriteLine($"bai 10 voi n = 1, ket qua: {SessionOne.bai10(1)}");
+Console.WriteLine($"bai 10 voi n = 0, ket qua: {SessionOne.bai10(0)}");
+Console.WriteLine($"bai 10 voi n = 9, ket qua: {SessionOne.bai10(9)}");
+Console.WriteLine();
+
+//
+Console.WriteLine($"bai 11 voi n = 1, ket qua: {SessionOne.bai11(1)}");
+Console.WriteLine($"bai 11 voi n = 0, ket qua: {SessionOne.bai11(0)}");
+Console.WriteLine($"bai 11 voi n = 9, ket qua: {SessionOne.bai11(9)}");
+Console.WriteLine();
+//
+Console.WriteLine($"bai 12 voi n = 1, x = 3 ket qua: {SessionOne.bai12(3,1)}");
+Console.WriteLine($"bai 12 voi n = 0, x = 3 ket qua: {SessionOne.bai12(3,0)}");
+Console.WriteLine($"bai 12 voi n = 9, x = 3 ket qua: {SessionOne.bai12(3,9)}");
+Console.WriteLine();
+//
+Console.WriteLine($"bai 12 voi n = 1, x = 3 ket qua: {SessionOne.bai12(3, 1)}");
+Console.WriteLine($"bai 12 voi n = 0, x = 3 ket qua: {SessionOne.bai12(3, 0)}");
+Console.WriteLine($"bai 12 voi n = 9, x = 3 ket qua: {SessionOne.bai12(3, 9)}");
+Console.WriteLine();
+//
+Console.WriteLine($"bai 13 voi n = 1, x = 3 ket qua: {SessionOne.bai13(3, 1)}");
+Console.WriteLine($"bai 13 voi n = 0, x = 3 ket qua: {SessionOne.bai13(3, 0)}");
+Console.WriteLine($"bai 13 voi n = 9, x = 3 ket qua: {SessionOne.bai13(3, 9)}");
+Console.WriteLine();
+//
+Console.WriteLine($"bai 14 voi n = 1, x = 3 ket qua: {SessionOne.bai14(3, 1)}");
+Console.WriteLine($"bai 14 voi n = 0, x = 3 ket qua: {SessionOne.bai14(3, 0)}");
+Console.WriteLine($"bai 14 voi n = 9, x = 3 ket qua: {SessionOne.bai14(3, 9)}");
+Console.WriteLine();
+//
+Console.WriteLine($"bai 15 voi n = 1, x = 3 ket qua: {SessionOne.bai15(1)}");
+Console.WriteLine($"bai 15 voi n = 0, x = 3 ket qua: {SessionOne.bai15(0)}");
+Console.WriteLine($"bai 15 voi n = 9, x = 3 ket qua: {SessionOne.bai15(9)}");
+Console.WriteLine();
+//
+Console.WriteLine($"bai 16 voi n = 1, x = 3 ket qua: {SessionOne.bai16(3, 1)}");
+Console.WriteLine($"bai 16 voi n = 0, x = 3 ket qua: {SessionOne.bai16(3, 0)}");
+Console.WriteLine($"bai 16 voi n = 9, x = 3 ket qua: {SessionOne.bai16(3, 9)}");
+Console.WriteLine();
 Console.ReadLine();
--- anhpm/SessionOne.cs
+++ anhpm/SessionOne.cs
@@ -107,4 +107,73 @@ public class SessionOne
             return 0;
         }
     }
+
+    public static int bai9(int n)
+    {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return n * bai9(n - 1);
+    }
+
+    public static double bai10(int n)
+    {
+
+        return Math.Pow(n, n);
+    }
+
+    public static int bai11(int n)
+    {
+        if (n == 1) return 1;
+        if (n == 0) return 0;
+        return bai9(n) + bai11(n - 1);
+    }
+
+    public static double bai12(int x, int n)
+    {
+        if (n == 1) return x;
+        if (n == 0) return 1;
+        return Math.Pow(x, n) + bai12(x, n - 1);
+    }
+
+    public static double bai13(int x, int n)
+    {
+        if (n == 1) return Math.Pow(x, 2);
+        if (n == 0) return 1;
+        return Math.Pow(x, n * 2) + bai13(x, n - 1);
+    }
+
+    public static double bai14(int x, int n)
+    {
+        if (n == 0) return x;
+        return Math.Pow(x, n * 2+1) + bai14(x, n - 1);
+    }
+
+    public static float bai15( int n)
+    {
+        try
+        {
+            if(n == 1) return 1;
+            if(n == 0)  throw new Exception("n = o is invalid");
+            return (float) 1f / bai1(n) + bai15(n - 1);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return 0;
+        }
+    }
+    public static float bai16(int x,int n)
+    {
+        try
+        {
+            if (n == 1) return x;
+            if (n == 0) throw new Exception("n = o is invalid");
+            return (float) (1f*Math.Pow(x,n) / bai1(n) + bai16(x,n - 1));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return 0;
+        }
+    }
 }
