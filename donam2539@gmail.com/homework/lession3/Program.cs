7549d9a8cfad008a9bb3e6996d683dc6df7b101e
namdbt00 <donam2539@gmail.com>
2024-08-28 00:07:36 +0700

    NamDBT to 40

--- donam2539@gmail.com/homework/lession3/Program.cs
+++ donam2539@gmail.com/homework/lession3/Program.cs
@@ -32,6 +32,79 @@ double bai35(int n)
     return result;
 }
 
+double bai36(int n)
+{
+    if (n == 0)
+        return 0;
+    double result = 1;
+    for (int i = 1; i < n; i++)
+    {
+        int value = 1;
+        for (int j = 1; j <= (i + 1); j++)
+        {
+            value *= j;
+        }
+        result = Math.Sqrt(value + result);
+    }
+    return result;
+}
+
+double bai37(int n)
+{
+    if (n == 0)
+        return 0;
+    double result = Math.Sqrt(2);
+    for (int i = 3; i <= n; i++)
+    {
+        result = Math.Pow((i + result), (double)1 / i);
+    }
+    return result;
+}
+
+double bai38(int n)
+{
+    if (n == 0)
+        return 0;
+    double result = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        result = Math.Pow((i + result), (double)1 / (i + 1));
+    }
+    return result;
+}
+
+double bai39(int n)
+{
+    if (n == 0)
+        return 0;
+    double result = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        int value = 1;
+        for (int j = 1; j <= i; j++)
+        {
+            value *= j;
+        }
+        result = Math.Pow((value + result), (double)1 / (i + 1));
+    }
+    return result;
+}
+
+double bai40(int n, int x)
+{
+    double result = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        result = Math.Sqrt(Math.Pow(x, i) + result);
+    }
+    return result;
+}
+
 Console.WriteLine("Bai 33: " + bai33(int.Parse(args[0])));
 Console.WriteLine("Bai 34: " + bai34(int.Parse(args[0])));
 Console.WriteLine("Bai 35: " + bai35(int.Parse(args[0])));
+Console.WriteLine("Bai 36: " + bai36(int.Parse(args[0])));
+Console.WriteLine("Bai 37: " + bai37(int.Parse(args[0])));
+Console.WriteLine("Bai 38: " + bai38(int.Parse(args[0])));
+Console.WriteLine("Bai 39: " + bai39(int.Parse(args[0])));
+Console.WriteLine("Bai 40: " + bai40(int.Parse(args[0]), int.Parse(args[1])));
