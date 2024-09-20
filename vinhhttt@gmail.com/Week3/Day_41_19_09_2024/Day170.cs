871663fb8fd84b6473841c6b91c60fb49bb6174e
vinhXD <vinhhttt@gmail.com>
2024-09-20 23:32:39 +0700

    do exersice

--- vinhhttt@gmail.com/Week3/Day_41_19_09_2024/Day170.cs
+++ vinhhttt@gmail.com/Week3/Day_41_19_09_2024/Day170.cs
@@ -1,6 +1,45 @@
-﻿namespace DefaultNamespace;
-
-public class Day170
+﻿namespace Week3.Day_41_18_09_2024
 {
-    
+    public class Bai170 : Homework
+    {
+        public override void Run()
+        {
+            int[] intArray = InputIntArray.GetInputIntArray();
+            int max = FindMax(intArray);
+            int nextPrime = FindNextPrime(max);
+            Console.WriteLine("So nguyen to nho nhat lon hon moi gia tri trong mang la: " + nextPrime);
+        }
+
+        private int FindMax(int[] array)
+        {
+            int max = array[0];
+            foreach (int num in array)
+            {
+                if (num > max)
+                    max = num;
+            }
+            return max;
+        }
+
+        private bool IsPrime(int n)
+        {
+            if (n < 2) return false;
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        private int FindNextPrime(int n)
+        {
+            int prime = n + 1;
+            while (!IsPrime(prime))
+            {
+                prime++;
+            }
+            return prime;
+        }
+    }
 }
--- vinhhttt@gmail.com/Week3/Day_41_19_09_2024/Day171.cs
+++ vinhhttt@gmail.com/Week3/Day_41_19_09_2024/Day171.cs
@@ -1,6 +1,25 @@
-﻿namespace DefaultNamespace;
-
-public class Day171
+﻿namespace Week3.Day_41_18_09_2024
+public class Bai171 : Homework
 {
-    
+    public override void Run()
+    {
+        int[] intArray = InputIntArray.GetInputIntArray();
+        int gcd = FindGCDArray(intArray);
+        Console.WriteLine("Uoc chung lon nhat cua cac phan tu trong mang la: " + gcd);
+    }
+
+    private int FindGCD(int a, int b)
+    {
+        return b == 0 ? a : FindGCD(b, a % b);
+    }
+
+    private int FindGCDArray(int[] array)
+    {
+        int gcd = array[0];
+        foreach (int num in array)
+        {
+            gcd = FindGCD(gcd, num);
+        }
+        return gcd;
+    }
 }
