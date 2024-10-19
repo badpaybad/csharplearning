077cb901752962964640fad1f3a0f52df6b7885c
dunp <badpaybad@gmail.com>
2024-10-19 13:15:51 +0700

    # Bài tập:
    Dùng các cấu trúc dữ liệu sẵn của c#: queue, dictionary , list ...
    để triển khai pub sub

--- session4/asynctest/Program.cs
+++ session4/asynctest/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System.Threading.Tasks;
 using System.Threading;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
@@ -12,8 +15,8 @@ while (true)
 
     Console.WriteLine($"============ {counter}");
 
-    await Log("DoSync",DoSync);
-    await Log("DoAsync",DoAsync);
+    await Log("DoSync", DoSync);
+    await Log("DoAsync", DoAsync);
 
     counter++;
 }
@@ -30,9 +33,9 @@ async Task Log(string tag, Func<Task> todo)
 
 async Task DoSync()
 {
-    await BocMy();
-    await NauNuoc();
-    await TronGiaVi();
+    var my = await BocMy();
+    var nc = await NauNuoc();
+    await TronGiaVi((long)my,(string) nc);
     await An();
 }
 
@@ -41,30 +44,65 @@ async Task DoAsync()
     var t1 = NauNuoc();
     var t2 = BocMy();
 
-    var t12 = Task.WhenAll(t1, t2);
+    var res = await Task.WhenAll(t1, t2);
 
-    await TronGiaVi();
+    await TronGiaVi((long)res[1], (string)res[0]);
     await An();
 }
 
-async Task BocMy()
+async Task<object> BocMy()
 {
-    await Task.Delay(1000);
+    await Task.Delay(3000);
     Console.WriteLine("BocMy done");
+    return DateTime.Now.Ticks;
 }
-async Task NauNuoc()
+async Task<object> NauNuoc()
 {
-    await Task.Delay(5000);
-    Console.WriteLine("NauNuoc done");
+    try
+    {
+        await Task.Delay(5000);
+        Console.WriteLine("NauNuoc done");
+        return "ok naunuoc " + DateTime.Now;
+    }
+    catch
+    {
+        return "";
+    }
 }
-async Task TronGiaVi()
+async Task TronGiaVi(long my, string nuoc)
 {
     await Task.Delay(2000);
-    Console.WriteLine("TronGiaVi done");
+    Console.WriteLine($"TronGiaVi done {my} {nuoc}");
 }
 
 async Task An()
 {
     await Task.Delay(3000);
     Console.WriteLine("An done");
+
+    var db =new AdsMongoDbContext();
+
 }
+
+  /// class này là tương đương 1 database
+            public class AdsMongoDbContext : BaseMongoObjectIdDbContext
+            {
+                public AdsMongoDbContext() : base(AppSettingsHelper.GetValueByKey("AdsMongoDbContext:ConnectionString"))
+                {
+
+                }
+
+                /// tương đương 1 collection
+                public DbSetObjectId<AdsPlaceEntity> AdsPlace { get; set; }
+
+            }
+
+
+            public class AdsPlaceEntity : AbstractEntityObjectIdTracking
+            {
+                public MongoDB.Bson.ObjectId UserOwnerId { get; set; }
+                public string PlaceName { get; set; }
+                public string PlaceAddress { get; set; }
+
+                public long LastUpdated{get;set;}
+            }
